1b1d2487c0744159d717c33f335cc24e1dd8eef9
Muhammad Sufyan <hafiz.sufyansadiq@gmail.com>
2024-09-04 18:11:11 +0500

    read kohli csv file

--- python-panda.py
+++ python-panda.py
@@ -1,1 +1,5 @@
-print("PANDA LEARNING HERE!..")
+import pandas as pd
+
+# Corrected file path using raw string
+ds = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T114815Z-001\datasets-session-16\kohli_ipl.csv')
+print(ds)
