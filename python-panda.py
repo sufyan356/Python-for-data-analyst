5a71d6da92e533a55d9520097d3c922c49d83538
Muhammad Sufyan <hafiz.sufyansadiq@gmail.com>
2024-09-05 02:49:39 +0500

    half video learn from campusx at 8/5/2024

--- python-panda.py
+++ python-panda.py
@@ -1,5 +1,73 @@
+import numpy as np
 import pandas as pd
+from numpy.ma.core import squeeze
 
-# Corrected file path using raw string
-ds = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T114815Z-001\datasets-session-16\kohli_ipl.csv')
-print(ds)
+### SERIES FROM LIST:
+# country = ['India' , 'Pakistan' , 'Nepal' , 'Afghanistan']
+# print(pd.Series(country))    # return --> index and value
+
+# marks = [100 , 90 , 80 , 70 , 60]
+# subjects = ['maths' , 'english' , 'chemistry' , 'physics' , 'urdu']
+# print(pd.Series(marks , index=subjects))   # custom indexing
+
+# marks = [100 , 90 , 80 , 70 , 60]
+# subjects = ['maths' , 'english' , 'chemistry' , 'physics' , 'urdu']
+# print(pd.Series(marks , index=subjects , name = "Sufyan's Marks"))   # naming
+
+### SERIES FROM DICTIONARY:
+# marks = {
+#     "maths" : 100,
+#     "english" : 90,
+#     "chemistry" : 80,
+#     "physics" : 70,
+#     "urdu" : 60,
+# }
+# marksSeriesDict = pd.Series (marks , name = "Sufyan's Marks")
+# print(marksSeriesDict)
+
+### SERIES ATTRIBUTES
+# size , dtype , name --> return col name , is_unique , index , values
+
+# marks = {
+#     "maths" : 100,
+#     "english" : 90,
+#     "chemistry" : 80,
+#     "physics" : 70,
+#     "urdu" : 60,
+# }
+# marksSeriesDict = pd.Series (marks , name = "Sufyan's Marks")
+# print("Size: " , marksSeriesDict.size)
+# print("Data Type: " , marksSeriesDict.dtype)
+# print("Name Att: " , marksSeriesDict.name)
+# print("Is Unique: " , marksSeriesDict.is_unique)
+# print("index: " , marksSeriesDict.index)
+# print("Size: " , marksSeriesDict.values)  #returns the numpy array
+
+### SERIES USING REAL WORLD DATA SET
+subs = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T114815Z-001\datasets-session-16\subs.csv').squeeze()
+# print(subs) #squeezed Fun return data in series
+
+vk = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T114815Z-001\datasets-session-16\kohli_ipl.csv' ,  index_col = 'match_no').squeeze()
+# print(vk)
+
+bw = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T114815Z-001\datasets-session-16\bollywood.csv' , index_col = 'movie').squeeze()
+# print(bw)
+
+### SERIES METHODS
+# head , tails , sample , value_counts , sort_values , sort_index
+# print(subs.head()) ###returns by default 5 top values
+# print(subs.head(10))
+
+# print(subs.tail())
+# print(subs.tail(10))
+
+# print(bw.sample())  ###returns by default 1 random value
+# print(bw.sample(3))
+
+# print(bw.value_counts())  ###returns no of movies by doing individual actors in descending order
+
+# print("Highest Runs By Virak Kohli: " , vk.sort_values(ascending = False).head(1).values[0]) ## does not sort the orignal data
+# print("Lowest Runs By Virak Kohli: " , vk.sort_values().head(1).values[0])
+
+# print(bw.sort_index())
+
