ee1358c002d5f9f754bbd2bbb6d36771a9920393
Muhammad Sufyan <hafiz.sufyansadiq@gmail.com>
2024-09-05 20:20:57 +0500

    almost complete 1st video on campusx

--- python-panda.py
+++ python-panda.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from numpy.ma.core import squeeze
+# from numpy.ma.core import squeeze
 
 ### SERIES FROM LIST:
 # country = ['India' , 'Pakistan' , 'Nepal' , 'Afghanistan']
@@ -10,8 +10,9 @@ from numpy.ma.core import squeeze
 # subjects = ['maths' , 'english' , 'chemistry' , 'physics' , 'urdu']
 # print(pd.Series(marks , index=subjects))   # custom indexing
 
-# marks = [100 , 90 , 80 , 70 , 60]
-# subjects = ['maths' , 'english' , 'chemistry' , 'physics' , 'urdu']
+marks = [100 , 90 , 80 , 70 , 60]
+subjects = ['maths' , 'english' , 'chemistry' , 'physics' , 'urdu']
+
 # print(pd.Series(marks , index=subjects , name = "Sufyan's Marks"))   # naming
 
 ### SERIES FROM DICTIONARY:
@@ -53,6 +54,7 @@ vk = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T11481
 bw = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T114815Z-001\datasets-session-16\bollywood.csv' , index_col = 'movie').squeeze()
 # print(bw)
 
+
 ### SERIES METHODS
 # head , tails , sample , value_counts , sort_values , sort_index
 # print(subs.head()) ###returns by default 5 top values
@@ -71,3 +73,66 @@ bw = pd.read_csv(r'C:\Users\Windows10\Desktop\datasets-session-16-20240904T11481
 
 # print(bw.sort_index())
 
+
+### SERIES MATHS METHODS
+# count --> null val does not count , sum , product , mean , median , mode , std , var , min , max , describe
+# print(subs.count())
+# print(subs.sum())
+# print(subs.product())
+# print(vk.mean())
+# print(subs.median())
+# print(bw.mode())
+# print(vk.std())
+# print(vk.var())
+# print(subs.min())
+# print(subs.max())
+# print(subs.describe())
+
+
+### SERIES INDEXING
+# directely negative indexing in panda not work.
+
+# x = pd.Series([1,3,2,5,4,7,6,9])
+# print(x.iloc[-1]) # return 9
+
+# y = pd.Series(["karachi","bulbulay","chocolate","donkey","elephant","flag"])
+# print(y[0]) # return karachi
+
+# print(bw.iloc[0]) # iloc used when index value is not set integer numbers in bw case index value is movie name that why we used iloc
+
+
+### SERIES SLICING
+# print(subs[0:101])
+# print(bw[-5:])
+
+### INDEXING WITH LABELS
+# print(bw['Hum Tumhare Hain Sanam'])
+
+### FANCY INDEXING
+# print(subs[[0,100,364]])
+
+
+### SERIES WITH PYTHON FUNCTIONALITIES
+# len , type , dir , sorted , max , min
+# type conversion
+# membership operator
+# looping
+# Arithmetic operator
+
+### len , type , dir , sorted , max , min
+# print(len(subs))
+# print(type(subs))
+# print(dir(subs))
+# print(sorted(subs)) #return in list form
+# print(max(subs))
+# print(min(subs))
+
+### membership operator (works in indexing val)
+# print(bw.head())
+
+# print('Battalion 609' in bw) #True
+# print('Vicky Ahuja' in bw)    #False
+# print('Vicky Ahuja' in bw.values)    #True
+
+
+###LOOPING
